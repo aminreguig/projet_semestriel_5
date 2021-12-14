f22391b0360b8ba1b0bdd809714976feb35ea376
aminreguig <55211675+aminreguig@users.noreply.github.com>
2021-12-14 22:46:16 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -17,6 +17,16 @@ st.write("""
 
 file = st.sidebar.file_uploader("Veuillez importer une image", type=["jpg", "png"])
 
+def transform(image):
+    
+        size = (224,224)    
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img_resize = (cv2.resize(img, size, interpolation=cv2.INTER_CUBIC))/255.
+        img_reshape = img_resize[np.newaxis,...]        
+        return img_reshape
+
         
         
 
@@ -24,6 +34,7 @@ file = st.sidebar.file_uploader("Veuillez importer une image", type=["jpg", "png
 if file is not None:
 
     image = Image.open(file)
+    transform(image)
     st.image(image, use_column_width=True)
     
     if (st.sidebar.button('extraire le numero')):
